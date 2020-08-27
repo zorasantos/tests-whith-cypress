6ae395afc76d1660b30cb662b3ecdbd63a04ec17
zoranildosantos <zoranildo22@gmail.com>
2020-08-27 20:47:58 -0300

    Testing update account and custom locators

--- cypress/integration/barriga/functional.spec.js
+++ cypress/integration/barriga/functional.spec.js
@@ -1,17 +1,29 @@
+import loc from '../../support/locators'
+
 describe('Should test at afunctional level', () => {
   before(() => {
     cy.visit('https://barrigareact.wcaquino.me/')
-    cy.get('[data-test=email]').type('jmaria1612@gmail.com')
-    cy.get('[data-test=passwd]').type('123456')
-    cy.get('.btn').click()
-    cy.get('.toast-message').should('contain', 'Bem vindo')
+    cy.get(loc.LOGIN.USER).type('jmaria1612@gmail.com')
+    cy.get(loc.LOGIN.PASSWORD).type('123456')
+    cy.get(loc.LOGIN.BTN_LOGIN).click()
+    cy.get(loc.MESSAGE).should('contain', 'Bem vindo')
   })
 
   it('Should create an account', () => {
-    cy.get('[data-test=menu-settings]').click()
-    cy.get('[href="/contas"]').click()
-    cy.get('[data-test=nome]').type('Conata Teste')
-    cy.get('.btn > .far').click()
-    cy.get('.toast-message').should('contain', 'Conta inserida com sucesso')
+    cy.get(loc.MENU.SETTINGS).click()
+    cy.get(loc.MENU.CONTAS).click()
+    cy.get(loc.CONTAS.NOME).type('Conta Teste')
+    cy.get(loc.CONTAS.BTN_SALVAR).click()
+    cy.get(loc.MESSAGE).should('contain', 'Conta inserida com sucesso')
+  })
+  it('Should update an account', () => {
+    cy.get(loc.MENU.SETTINGS).click()
+    cy.get(loc.MENU.CONTAS).click()
+    cy.get(':nth-child(1) > :nth-child(2) > .fa-edit').click()
+    cy.get(loc.CONTAS.NOME)
+      .clear()
+      .type('Conta Teste Alterada[2]')
+    cy.get(loc.CONTAS.BTN_SALVAR).click()
+    cy.get(loc.MESSAGE).should('contain', 'Conta atualizada com sucesso')
   })
 })
