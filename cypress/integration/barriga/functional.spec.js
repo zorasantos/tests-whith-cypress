c3b816d716ea90d1cf9c159dc9c805b1ae836b4f
zoranildosantos <zoranildo22@gmail.com>
2020-09-07 20:58:09 -0300

    Create transaction

--- cypress/integration/barriga/functional.spec.js
+++ cypress/integration/barriga/functional.spec.js
@@ -1,4 +1,5 @@
 import loc from '../../support/locators'
+import '../../support/commandsAccounts'
 
 describe('Should test at afunctional level', () => {
   before(() => {
@@ -6,20 +7,35 @@ describe('Should test at afunctional level', () => {
   })
 
   it('Should create an account', () => {
-    cy.get(loc.MENU.SETTINGS).click()
-    cy.get(loc.MENU.CONTAS).click()
-    cy.get(loc.CONTAS.NOME).type('Conta Teste')
-    cy.get(loc.CONTAS.BTN_SALVAR).click()
+    cy.acessarMenuConta()
+    cy.inserirConta('Conta Teste')
     cy.get(loc.MESSAGE).should('contain', 'Conta inserida com sucesso')
   })
+
   it('Should update an account', () => {
-    cy.get(loc.MENU.SETTINGS).click()
-    cy.get(loc.MENU.CONTAS).click()
+    cy.acessarMenuConta()
     cy.get(':nth-child(1) > :nth-child(2) > .fa-edit').click()
     cy.get(loc.CONTAS.NOME)
       .clear()
       .type('Conta Teste Alterada[2]')
     cy.get(loc.CONTAS.BTN_SALVAR).click()
     cy.get(loc.MESSAGE).should('contain', 'Conta atualizada com sucesso')
+  })
+
+  it('Should not create an account with same name', () => {
+    cy.acessarMenuConta()
+    cy.inserirConta('Conta Teste Alterada[2]')
+    cy.get(loc.MESSAGE).should('contain', 'code 400')
+  })
+
+  it('Should create a transaction', () => {
+    cy.get(loc.MENU.MOVIMENTACAO).click()
+    cy.get(loc.MOVIMENTACAO.DESCRICAO).type('Descrição')
+    cy.get(loc.MOVIMENTACAO.VALOR).type('123')
+    cy.get(loc.MOVIMENTACAO.INTERESSADO).type('Interessado')
+    cy.get(loc.MOVIMENTACAO.BTN_SALVAR).click()
+    cy.get(loc.MESSAGE).should('contain', 'Movimentação inserida com sucesso')
+
+    cy.get(loc.EXTRATO.LINHAS).should('have.length', 7)
   })
 })
--- cypress/support/locators.js
+++ cypress/support/locators.js
@@ -7,11 +7,21 @@ const locators = {
   MENU: {
     SETTINGS: '[data-test=menu-settings]',
     CONTAS: '[href="/contas"]',
-    RESET: '[href="/reset"]'
+    RESET: '[href="/reset"]',
+    MOVIMENTACAO: '[data-test=menu-movimentacao]'
   },
   CONTAS: {
     NOME: '[data-test=nome]',
     BTN_SALVAR: '.btn'
+  },
+  MOVIMENTACAO: {
+    DESCRICAO: '[data-test=descricao]',
+    VALOR: '[data-test=valor]',
+    INTERESSADO: '[data-test=envolvido]',
+    BTN_SALVAR: '.btn-primary'
+  },
+  EXTRATO: {
+    LINHAS: '.list-group > li'
   },
   MESSAGE: '.toast-message'
 }
